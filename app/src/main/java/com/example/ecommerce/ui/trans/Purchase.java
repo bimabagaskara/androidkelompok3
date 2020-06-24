04aa21cf3c1244c04835fbe56f30a004c80b4584
AnaFarida <anafarida1820@gmail.com>
2020-06-24 14:50:48 +0700

    Membuat Pembelia

--- app/src/main/java/com/example/ecommerce/ui/trans/Purchase.java
+++ app/src/main/java/com/example/ecommerce/ui/trans/Purchase.java
@@ -7,60 +7,59 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.ecommerce.MainActivity;
 import com.example.ecommerce.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Purchase#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 public class Purchase extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ListView lstPurchase;
+    SimpleAdapter simpleAdapter;
+    ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
 
     public Purchase() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Purchase.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Purchase newInstance(String param1, String param2) {
-        Purchase fragment = new Purchase();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_purchase, container, false);
+        View view = inflater.inflate(R.layout.fragment_purchase, container, false);
+        lstPurchase = (ListView) view.findViewById(R.id.lstPurchase);
+        setPurchase();
+
+        return view;
+    }
+
+    private void setPurchase() {
+        for (int i = 1; i <= 10; i++) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("price",  "Rp 100.000");
+            arrayList.add(map);
+
+        }
+
+        simpleAdapter = new SimpleAdapter(getActivity(), arrayList, R.layout.adapter_trans,
+                new String[]{"price"},
+                new int[] {R.id.txtDesc});
+        lstPurchase.setAdapter(simpleAdapter);
+        lstPurchase.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String harga = ((TextView) view.findViewById(R.id.txtDesc)).getText().toString();
+                Toast.makeText(getActivity(), harga, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
